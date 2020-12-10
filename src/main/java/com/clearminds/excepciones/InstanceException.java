371ac46d98ea4a70563364021ce196e2d3828f66
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-10 10:33:31 -0500

    InstanceException implementada

---
+++ src/main/java/com/clearminds/excepciones/InstanceException.java
@@ -0,0 +1,14 @@
+package com.clearminds.excepciones;
+
+public class InstanceException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1457041960302559913L;
+
+	public InstanceException(String mensaje) {
+		super(mensaje);
+	}
+
+}
--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -1,5 +1,6 @@
 package com.clearminds.impl;
 
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
 
@@ -8,7 +9,21 @@ public class PersonaManager {
 	private ServicioPersona serv;
 	
 	public PersonaManager() {
-		serv = new ServicioPersonaBDD();
+		Class<?> clase;
+		try {
+			clase = Class.forName("com.clearminds.impl.ServicioPersonaBDD");
+			serv=(ServicioPersona)clase.newInstance();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		}
+
 	}
 
 	public ServicioPersona getServ() {
@@ -20,7 +35,6 @@ public class PersonaManager {
 	}
 	
 	public void insertarPersona(Persona persona) {
-		serv = new ServicioPersonaArchivos();
 		serv.insertar(persona);
 	}
 
