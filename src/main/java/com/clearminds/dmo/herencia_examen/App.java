43d855b1379f06737e730e0a21874523d62324e3
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-10 10:03:12 -0500

    Versio Inicial

---
+++ src/main/java/com/clearminds/dmo/herencia_examen/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.dmo.herencia_examen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
